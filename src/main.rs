80cef61067c62e19551c300d5f33ae2b8106d34c
filipton <filipton12@gmail.com>
2024-11-14 16:45:47 +0100

    explore: more registers

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use esp32c3::{
+    apb_saradc::{tsens_ctrl, TSENS_CTRL},
+    APB_SARADC,
+};
 use esp_backtrace as _;
 use esp_hal::prelude::*;
 use log::info;
@@ -18,15 +22,18 @@ async fn main(_spawner: Spawner) {
     });
 
     esp_alloc::heap_allocator!(72 * 1024);
-
     esp_println::logger::init_logger_from_env();
 
     let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
+    let apb_saradc = unsafe { &*APB_SARADC::PTR };
+    apb_saradc.tsens_ctrl().modify(|_, w| w.pu().bit(true));
+    apb_saradc.tsens_ctrl2().modify(|_, w| w.clk_sel().bit(false));
 
     let mut counter = 0;
     loop {
-        info!("Hello world! {counter}");
+        let r = apb_saradc.tsens_ctrl().read().out().bits();
+        info!("Hello world! {counter} {r}");
         counter += 1;
         Timer::after(Duration::from_millis(100)).await;
     }
